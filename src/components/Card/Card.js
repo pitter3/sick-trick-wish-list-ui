a0ea1e4305fb34108d0ec379a9c484e66b2d13ba
Patrick Eitel <patrickeitel12@gmail.com>
2023-10-30 09:08:54 -0700

    import useEf fect and useState, setup base state for tricks, and implement a network request and useEffect to update that state

---
+++ src/components/Card/Card.js
@@ -0,0 +1,9 @@
+import "./Card.css"
+
+function Card() {
+  return (
+    <h1 className="card">Card Component</h1>
+  )
+}
+
+export default Card
---
+++ src/components/Form/Form.js
@@ -0,0 +1,9 @@
+import "./Form.css"
+
+function Form() {
+  return (
+    <h1 className="form">Form Component</h1>
+  )
+}
+
+export default Form
