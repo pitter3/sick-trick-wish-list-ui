360fe4993f72f8a71fd927c321ce44cc7fe92a6d
Patrick Eitel <patrickeitel12@gmail.com>
2023-10-30 09:18:08 -0700

    implement Tricks.js mapping logic to create a trick card for each trick in the state

--- src/components/Tricks/Tricks.js
+++ src/components/Tricks/Tricks.js
@@ -1,8 +1,26 @@
 import "./Tricks.css"
+import Card from "../Card/Card"
 
-function Tricks() {
+function Tricks({tricks}) {
+
+  const allCards = tricks.map((trick) => {
+    return (
+      <Card 
+      key={trick.id}
+      id={trick.id}
+      name={trick.name}
+      stance={trick.stance}
+      obstacle={trick.obstacle}
+      tutorial={trick.tutorial}
+      />
+    )
+  })
+
+  
   return (
-    <h1 className="tricks">Tricks Component</h1>
+    <div className="tricks">
+      {allCards}
+    </div>
   )
 }
 
